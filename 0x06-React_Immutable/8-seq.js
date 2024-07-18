35da6f1bdfd7398fd4a4099bc76ca2c2f8181cf2
Naasor <amofasandra7@gmail.com>
2024-07-18 02:30:20 +0000

    Lasy Seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,27 @@
+import { Seq } from 'immutable';
+
+/**
+ * Capitalizes the first letter of a string
+ */
+function formatName(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function printBestStudents(object) {
+  const seq = Seq(object);
+
+  const filteredStudents = seq.filter(function(value) {
+    return value.score > 70;
+  });
+
+  const students = filteredStudents.toJS();
+
+  Object.keys(students).forEach(function(key) {
+    students[key].firstName = formatName(students[key].firstName);
+    students[key].lastName = formatName(students[key].lastName);
+  });
+
+  console.log(students);
+}
+
+export default printBestStudents;
